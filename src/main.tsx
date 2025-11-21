5fcb45a9a2c095bb14004e448381651472675f68
gabrielbrrll <gabrielb.ocampo@gmail.com>
2025-11-21 20:50:09 +0400

    chore: add restaurant-themed Ionic color palette and clean up CSS
    - Create theme/variables.css with warm, inviting restaurant colors
    - Simplify App.css and index.css for Ionic app
    - Install @types/react-router-dom for TypeScript compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+import './theme/variables.css';
 import './index.css';
 import App from './App.tsx';
 
